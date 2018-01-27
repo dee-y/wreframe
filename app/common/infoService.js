6186fc1d05770d929021a02a29c60db50090bc3d
Pradhaban <pradhabstrikes@gmail.com>
2018-01-28 03:14:01 +0530

    New Tool and some intialization
    Scroll bar has been introduced and info Service has been intitiated

---
+++ app/common/infoService.js
@@ -0,0 +1,17 @@
+/* 
+ * Modal and Notification Service
+ */
+
+(function(){
+    
+    'use strict';
+    
+    function infoService(){
+        
+    };
+    
+    
+    angular.module("freehand").service("infoService", [infoService]);
+})();
+
+
