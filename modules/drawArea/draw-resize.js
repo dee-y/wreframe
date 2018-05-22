768aa666dea9a1fc08a3f36f89c24a51c8ffbca0
Pradhaban <pradhabstrikes@gmail.com>
2018-05-22 09:48:23 +0530

    Jquery Included & Canvas Resize
    Canvas Resize functionality with Jquery UI

--- modules/drawArea/draw-resize.js
+++ modules/drawArea/draw-resize.js
@@ -8,51 +8,19 @@
     var app = angular.module('wreframe.draw.resize', []);
 
     function resize() {
-
         var vm = this;
-        vm.rightHandle={mouseDown:false,mouseUp:false,mouseMove:false};
-        vm.downHandle={mouseDown:false,mouseUp:false,mouseMove:false};
-        vm.divStyle={divWidth:null,divHeight:null};
-        
-        vm.init = function(){
-            vm.divStyle.divWidth = window.innerWidth * 0.9 +'px';
-            vm.divStyle.divHeight = window.innerHeight * 0.9 +'px';
-        }
-        
-        vm.mouseUp = function(val){
-            if(val === "right"){
-                vm.rightHandle.mouseUp=true;
-            }
-            if(val === "down"){
-                vm.downHandle.mouseUp=true;
-            }
-            console.log('Mouse UP',vm.rightHandle.mouseUp.toString());
-        };
-        
-        vm.mouseDown = function(val){
-            if(val === "right"){
-                vm.rightHandle.mouseDown=true;
-            }
-            if(val === "down"){
-                vm.downHandle.mouseDown=true;
-            }
-            console.log('Mouse DOWN',vm.rightHandle.mouseDown.toString());
-        };
-        
-        vm.mouseMove = function (val) {
-            if (val === "right" && vm.rightHandle.mouseDown === true) {
-                vm.rightHandle.mouseMove = true;
-                var divWidth = parseInt(vm.divStyle.divWidth);
-                divWidth--;
-                vm.divStyle.divWidth = divWidth + 'px';
-            }
-            if (val === "down" && vm.downHandle.mouseDown === true) {
-                vm.downHandle.mouseMove = true;
-            }
+        vm.drawArea = {dwidth: 0, dheight: 0};
+        vm.resize = false;
+        vm.startX = 0;
+        vm.startY = 0;
+
+        vm.init = function () {
+            vm.drawArea.dwidth = window.innerWidth * 0.9 + 'px';
+            vm.drawArea.dheight = window.innerHeight * 0.9 + 'px';
+            $("#drawArea").resizable();
         };
     }
     ;
     app.controller('drawResize', resize);
 
 })();
-
