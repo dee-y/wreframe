35dade9f000dcc99120465de92e307ac0b85656d
Pradhaban <pradhabstrikes@gmail.com>
2018-05-24 04:02:21 +0530

    Layout Fix in Progress
    Layout Fixes with adjust to Sidebar

---
+++ modules/shapes/shapes.js
@@ -0,0 +1,17 @@
+/* 
+ * Shapes Functionality Goes Here
+ */
+
+(function(){
+    
+    'use strict';
+    
+    function shapeSelector(){
+        
+        var vm=this;
+        
+    };
+    
+    angular.module('wreframe').controller('shapesSelector',shapeSelector);
+    
+})();
