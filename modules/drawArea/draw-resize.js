eaa223e9a23003b763149a67df74e55604b8badb
Pradhaban <pradhabstrikes@gmail.com>
2018-05-28 22:52:25 +0530

    Layout Fix
    :balloon:  fixed layout.. changes in CSS and JS

--- modules/drawArea/draw-resize.js
+++ modules/drawArea/draw-resize.js
@@ -7,29 +7,34 @@
 
     var app = angular.module('wreframe.draw.resize', []);
 
-    function resize() {
+    function resize($timeout) {
         var vm = this;
-        vm.drawArea = {dwidth: 0, dheight: 0};
+        vm.drawArea = {dwidth: 0, dheight: 0 , mWidth:0 , mHeight:0};
         vm.resize = false;
         vm.startX = 0;
         vm.startY = 0;
 
         vm.init = function () {
-            var maxWidth=window.innerWidth;
-            var maxHeight=window.innerHeight;
-            vm.drawArea.dwidth =  maxWidth+ 'px';
-            vm.drawArea.dheight = maxHeight+ 'px';
-            $("#drawArea").resizable(
-                    {
-                        maxHeight: maxHeight,
-                        maxWidth: maxWidth,
-                        minHeight: 200,
-                        minWidth: 200
-                    }
-            );
+            var maxWidth = window.screen.availWidth;
+            var maxHeight = window.screen.availHeight;
+            $timeout(function () {
+                if (maxWidth > 300) {
+                    vm.drawArea.mWidth =  vm.drawArea.dwidth = (maxWidth * 0.8) + 'px';
+                    vm.drawArea.mHeight = vm.drawArea.dheight =(maxHeight * 0.8) + 'px';
+                    $("#drawArea").resizable(
+                            {
+                                maxHeight: (maxHeight * 0.8),
+                                maxWidth: (maxWidth * 0.8),
+                                minHeight: 200,
+                                minWidth: 200
+                            }
+                    );
+                }
+            }, 1000);
+
         };
     }
     ;
-    app.controller('drawResize', resize);
+    app.controller('drawResize', ['$timeout',resize]);
 
 })();
