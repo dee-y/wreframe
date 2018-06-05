bf7313af0521222476a9818761ed1a3de8fcddd0
Pradhaban <pradhabstrikes@gmail.com>
2018-06-05 23:14:13 +0530

    Code Refactor and Zoom icon
    Functions has been code refactor and zoom icon has been implemented

--- modules/drawArea/draw-resize.js
+++ modules/drawArea/draw-resize.js
@@ -9,19 +9,20 @@
 
     function resize($timeout) {
         var vm = this;
-        vm.drawArea = {dwidth: 0, dheight: 0 , mWidth:0 , mHeight:0};
+        vm.drawArea = {dwidth: 0, dheight: 0, mWidth: 0, mHeight: 0};
         vm.resize = false;
         vm.startX = 0;
         vm.startY = 0;
-        vm.resizePercent=0.85;
+        vm.resizePercent = 0.85;
+        vm.divs = {};
 
         vm.init = function () {
             var maxWidth = window.screen.availWidth;
             var maxHeight = window.screen.availHeight;
             $timeout(function () {
                 if (maxWidth > 300) {
-                    vm.drawArea.mWidth =  vm.drawArea.dwidth = (maxWidth * vm.resizePercent) + 'px';
-                    vm.drawArea.mHeight = vm.drawArea.dheight =(maxHeight * vm.resizePercent) + 'px';
+                    vm.drawArea.mWidth = vm.drawArea.dwidth = (maxWidth * vm.resizePercent) + 'px';
+                    vm.drawArea.mHeight = vm.drawArea.dheight = (maxHeight * vm.resizePercent) + 'px';
                     $("#drawArea").resizable(
                             {
                                 maxHeight: (maxHeight * vm.resizePercent),
@@ -30,12 +31,27 @@
                                 minWidth: 200
                             }
                     );
+                    initializeDrawArea();
                 }
             }, 1000);
 
         };
+
+
+        var initializeDrawArea = function () {
+            $("#drawArea").selectable({
+                stop: function (event, ui) {
+                 console.log(event);
+                    console.log(ui);   
+                }
+            });
+            $( "#drawArea" ).on( "selectableselected", function( event, ui ) {
+                console.log(event);
+                    console.log(ui);
+            } );
+        };
     }
     ;
-    app.controller('drawResize', ['$timeout',resize]);
+    app.controller('drawResize', ['$timeout', resize]);
 
 })();
